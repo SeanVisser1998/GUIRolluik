bd37856f6d51bc43c75ed6f24e0c153c44fd5d65
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-11-05 00:00:58 +0100

    status opvragen van een rolluik

--- GUIRolluik/model/rolluik.py
+++ GUIRolluik/model/rolluik.py
@@ -6,7 +6,19 @@ Created on 4 Nov 2018
 import serial
 from model import serialSettings as settings
 
+# Status opvragen enzo
 
+def getStatus(rolluiknummer):
+    comport = settings.rolluikPoort[rolluiknummer -1] #-1 omdat eenheid 1 is index 0
+    serial = serial.Serial(comport=comport, baudrate = settings.baudrate, timeout = settings.timeout)
+    serial.Write(b'hihi') #moet ook 'hihi' zijn in C om rolluik status op te vragen
+    status = serial.readline.decode('ascii')
+    if status == 1:
+        return 'open' 
+    else:
+        return 'dicht'
+
+# Open / sluit shizle
 def sluitAlle():
     for comport in settings.rolluikPoort:
         serial = serial.Serial(comport = comport, baudrate = settings.baudrate, timeout = settings.timeout)
