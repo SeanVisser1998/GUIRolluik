aa5bfd64f55bfea727cf69eedb74f4feaf314a00
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-11-04 23:53:32 +0100

    Rolluik selecteer fix =)

--- GUIRolluik/model/rolluik.py
+++ GUIRolluik/model/rolluik.py
@@ -18,11 +18,11 @@ def openAlle():
         serial.write(b'open') #moet ook 'open' zijn in C om rolluik te openen
 
 def sluitRolluik(rolluiknummer):
-    comport = settings.rolluikPoort[rolluiknummer +1]
+    comport = settings.rolluikPoort[rolluiknummer -1] #-1 omdat eenheid 1 is index 0
     serial = serial.Serial(comport=comport, baudrate = settings.baudrate, timeout = settings.timeout)
     serial.Write(b'sluit') #moet ook 'sluit' zijn in C om rolluik te sluiten
 
 def openRolluik(rolluiknummer):
-    comport = settings.rolluikPoort[rolluiknummer +1]
+    comport = settings.rolluikPoort[rolluiknummer -1] #-1 omdat eenheid 1 is index 0
     serial = serial.Serial(comport=comport, baudrate = settings.baudrate, timeout = settings.timeout)
     serial.Write(b'open') #moet ook 'open' zijn in C om rolluik te openen
