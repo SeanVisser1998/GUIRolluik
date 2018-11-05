f4e5550af5e69f541c9a909b5562cf5aa2e0b132
Sean <s.l.visser@st.hanze.nl>
2018-11-05 17:56:11 +0100

    Serial communication fix

--- GUIRolluik/model/temperatuursensor.py
+++ GUIRolluik/model/temperatuursensor.py
@@ -6,7 +6,7 @@ Created on 4 Nov 2018
 import serial
 from model import serialSettings as settings
 
-serial = serial.Serial(settings.comPort, baudrate = settings.baudrate, timeout = settings.timeout)
+serial = serial.Serial(settings.sensorPort, baudrate = settings.baudrate, timeout = settings.timeout)
 
 def getTemperatuur():
     serial.write(b'temperatauur') #Moet ook licht zijn in C-code
