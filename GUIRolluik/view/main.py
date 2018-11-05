66632581a3910234215c768f1ffb31c3321fc4d9
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-11-05 19:58:34 +0100

    Update frame fix :)

--- GUIRolluik/view/main.py
+++ GUIRolluik/view/main.py
@@ -56,58 +56,78 @@ class mainGUI(Frame):
         lichtL.grid(column=3, row=4)
         
         #Knoppen genereren :3
-        rowI = 5 #begint bij Row 5 ofzo =)
+        rowI = 5
         for rolluik in range(len(serialSettings.rolluikNaam)):
+            
 
             #Rolluik
-            eenheid = ttk.Button(self, text=serialSettings.rolluikNaam[rolluik])
-            eenheid.grid(column=0, row=rowI, ipady=5, ipadx=15, padx=5, pady=5)
+            eenheid1 = ttk.Button(self, text=serialSettings.rolluikNaam[rolluik])
+            eenheid1.grid(column=0, row=rowI, ipady=5, ipadx=15, padx=5, pady=5)
             
-            #Status
-            status = Button(self,bg=serialSettings.status[rolluik])
-            status.grid(column=1, row=rowI, ipady=1, ipadx=9, padx=5, pady=5)
+    
             
             #Temperatuur
-            temperatuur = Label(self,text="10 Graden", background="white")
-            temperatuur.grid(column=2, row=rowI, pady=5, padx=5)
+            temperatuur1 = Label(self,text="10 Graden", background="white")
+            temperatuur1.grid(column=2, row=rowI, pady=5, padx=5)
             
             #Lichtintensiteit
-            lichtintensiteit = Label(self, text="143 RARE UNITS", background="white")
-            lichtintensiteit.grid(column=3, row=rowI, pady=5, padx=5)            
-           
-            #Row increment :3
-            rowI = rowI + 1
-            status.config(bg=serialSettings.status[rolluik])
+            lichtintensiteit1 = Label(self, text="143 RARE UNITS", background="white")
+            lichtintensiteit1.grid(column=3, row=rowI, pady=5, padx=5)            
             
+            rowI +=1
+            
+        #Combined functions
+        def combine_funcs(*funcs):
+            def combined_func(*args, **kwargs):
+                for f in funcs:
+                    f(*args, **kwargs)
+            return combined_func
+        
+        #Status
+        status1 = Button(self,bg=serialSettings.status[0])
+        status1.grid(column=1, row=5, ipady=1, ipadx=9, padx=5, pady=5)
+        
+        status2 = Button(self,bg=serialSettings.status[1])
+        status2.grid(column=1, row=6, ipady=1, ipadx=9, padx=5, pady=5)
+        
+        status3 = Button(self,bg=serialSettings.status[2])
+        status3.grid(column=1, row=7, ipady=1, ipadx=9, padx=5, pady=5)
+        
+        status4 = Button(self,bg=serialSettings.status[3])
+        status4.grid(column=1, row=8, ipady=1, ipadx=9, padx=5, pady=5)
+        
+        status5 = Button(self,bg=serialSettings.status[4])
+        status5.grid(column=1, row=9, ipady=1, ipadx=9, padx=5, pady=5)
+        
         #Controle
-        omhoog1 = ttk.Button(self, text=u"\u25B2", command=lambda : sesam.openRolluik(0))
+        omhoog1 = ttk.Button(self, text=u"\u25B2", command=lambda : combine_funcs(sesam.openRolluik(0), status1.config(background=serialSettings.status[0])))
         omhoog1.grid(column=4, row=5)
             
-        omlaag1 = ttk.Button(self, text=u"\u25BC", command=lambda : sesam.sluitRolluik(0))
+        omlaag1 = ttk.Button(self, text=u"\u25BC", command=lambda : combine_funcs(sesam.sluitRolluik(0), status1.config(background=serialSettings.status[0])))
         omlaag1.grid(column=5, row=5)
         
-        omhoog2 = ttk.Button(self, text=u"\u25B2", command=lambda : sesam.openRolluik(1))
+        omhoog2 = ttk.Button(self, text=u"\u25B2", command=lambda : combine_funcs(sesam.openRolluik(1), status2.config(background=serialSettings.status[1])))
         omhoog2.grid(column=4, row=6)
             
-        omlaag2 = ttk.Button(self, text=u"\u25BC", command=lambda : sesam.sluitRolluik(1))
+        omlaag2 = ttk.Button(self, text=u"\u25BC", command=lambda : combine_funcs(sesam.sluitRolluik(1), status2.config(background=serialSettings.status[1])))
         omlaag2.grid(column=5, row=6)
     
-        omhoog3 = ttk.Button(self, text=u"\u25B2", command=lambda : sesam.openRolluik(2))
+        omhoog3 = ttk.Button(self, text=u"\u25B2", command=lambda : combine_funcs(sesam.openRolluik(2), status3.config(background=serialSettings.status[2])))
         omhoog3.grid(column=4, row=7)
             
-        omlaag3 = ttk.Button(self, text=u"\u25BC", command=lambda : sesam.sluitRolluik(2))
+        omlaag3 = ttk.Button(self, text=u"\u25BC", command=lambda : combine_funcs(sesam.sluitRolluik(2), status3.config(background=serialSettings.status[2])))
         omlaag3.grid(column=5, row=7)
         
-        omhoog4 = ttk.Button(self, text=u"\u25B2", command=lambda : sesam.openRolluik(3))
+        omhoog4 = ttk.Button(self, text=u"\u25B2", command=lambda : combine_funcs(sesam.openRolluik(3), status4.config(background=serialSettings.status[3])))
         omhoog4.grid(column=4, row=8)
             
-        omlaag4 = ttk.Button(self, text=u"\u25BC", command=lambda : sesam.sluitRolluik(3))
+        omlaag4 = ttk.Button(self, text=u"\u25BC", command=lambda : combine_funcs(sesam.sluitRolluik(3), status4.config(background=serialSettings.status[3])))
         omlaag4.grid(column=5, row=8)
         
-        omhoog5 = ttk.Button(self, text=u"\u25B2", command=lambda : sesam.openRolluik(4))
+        omhoog5 = ttk.Button(self, text=u"\u25B2", command=lambda : combine_funcs(sesam.openRolluik(4), status5.config(background=serialSettings.status[4])))
         omhoog5.grid(column=4, row=9)
             
-        omlaag5 = ttk.Button(self, text=u"\u25BC", command=lambda : sesam.sluitRolluik(4))
+        omlaag5 = ttk.Button(self, text=u"\u25BC", command=lambda : combine_funcs(sesam.sluitRolluik(4), status5.config(background=serialSettings.status[4])))
         omlaag5.grid(column=5, row=9)
         
 
