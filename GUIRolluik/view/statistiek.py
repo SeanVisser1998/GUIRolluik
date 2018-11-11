f56dc801559a51c036fce7251db2cf6a6a76baa9
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-11-11 21:18:01 +0100

    Live grafiek update

--- GUIRolluik/view/statistiek.py
+++ GUIRolluik/view/statistiek.py
@@ -12,6 +12,18 @@ from tkinter import ttk
 from view import main
 
 from model import serialSettings
+
+import matplotlib
+matplotlib.use("TKAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+
+#Global Variables
+f = Figure(figsize=(1.5,1.5),dpi=100)
+a = f.add_subplot(111)
+
+
 class statistiekGUI(Frame):
     '''
         In serialSettings is een array temp
@@ -31,8 +43,29 @@ class statistiekGUI(Frame):
     
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        label = Label(self, text="Statistiek", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
+        label = Label(self, text="Statistiek", font=LARGE_FONT,background="white")
+        label.pack()
         
         knop1 = ttk.Button(self, text="Terug", command=lambda: controller.show(main.mainGUI))
-        knop1.pack() 
+        knop1.pack()
+                     
+
+        
+        self.update()
+
+    def update(self):
+        
+        global f
+        global a
+        
+        a.clear()
+        a.bar([1,2,3,4,5],[serialSettings.temp[0],serialSettings.temp[1],serialSettings.temp[2],serialSettings.temp[3],serialSettings.temp[4]],color='orange')       
+
+        canvas = FigureCanvasTkAgg(f,self)
+        canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH,expand=True)
+        canvas.draw()
+            
+        print("Hello")
+           
+        self.after(2000,self.update)
+            
