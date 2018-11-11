c0afe89a7b5387d75c99e557443b48a9ce4242b2
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-11-11 21:19:43 +0100

    Live testing functie toegevoegd
    Functie die het mogelijk maakt om te testen of het live updaten ook zichtbaar is in de GUI

--- GUIRolluik/model/temperatuursensor.py
+++ GUIRolluik/model/temperatuursensor.py
@@ -7,8 +7,14 @@ import serial
 from model import serialSettings as settings
 from model import rolluik as sesam
 
+
+from random import randint
 def calcTemp(temp):
     return (temp * (140/255)) -40
+
+def liveTest():
+    i = randint(20,30)
+    settings.temp[0] = i
 
 def getTemperatuurArduino(rolluik):
     comport=settings.rolluikDict.get(rolluik)
